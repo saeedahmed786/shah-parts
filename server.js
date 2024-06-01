7f735c7e9404760cfa1613b03552c2cf84ad981b
saeedahmed786 <119362834+devuplio@users.noreply.github.com>
2024-06-01 06:21:11 +0500

    updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const morgan = require('morgan');
 const app = express();
 
 /******************************************  MiddleWares  ********************************************/
-app.use(express.json({ limit: "1000mb" }));
+app.use(express.json({ limit: "500mb" }));
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended: true }));
